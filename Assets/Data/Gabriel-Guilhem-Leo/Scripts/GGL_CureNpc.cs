d513d052f357174532343a782c75d19aec14912e
L3o <leogravillon@hotmail.fr>
2023-01-12 11:36:32 +0100

    [refactor] thank you message

--- Assets/Data/Gabriel-Guilhem-Leo/Scripts/GGL_CureNpc.cs
+++ Assets/Data/Gabriel-Guilhem-Leo/Scripts/GGL_CureNpc.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GGL_CureNpc : Interactive
 {
     public Collider interactionCollider;
+    [SerializeField] private GameObject thankYouPanel;
+    [SerializeField] private TextMeshProUGUI thankYou;
+    [SerializeField] private Button welcome;
+    [SerializeField] private string thankYouMessage;
+
+    private void Start()
+    {
+        welcome.onClick.AddListener(delegate
+        {
+            thankYouPanel.SetActive(false);
+            Time.timeScale = 1;
+        });
+    }
     public override void OnInteraction()
     {
         if (Inventory.Instance.IsItemFound(requiredItems[0].item))
@@ -12,5 +27,13 @@ public class GGL_CureNpc : Interactive
             Inventory.Instance.RemoveFromInventory(requiredItems[0].item);
             if (onlyOnce) interactionCollider.enabled = false;
         }
+    }
+
+    public void ThankYou()
+    {
+        thankYou.text = thankYouMessage;
+        thankYouPanel.SetActive(true);
+        Time.timeScale = 0;
+        welcome.Select();
     }
 }
